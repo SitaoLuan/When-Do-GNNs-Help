0ab556eb5ebbbdfe634cea6a5ba932ca3a1883d9
jzhu1905 <jiaqi.zhu@mail.mcgill.ca>
2023-12-15 12:55:16 -0500

    cleanup synthetic_plot.py

--- utils.py
+++ utils.py
@@ -1,31 +1,16 @@
+import os
+import pickle as pkl
+import sys
+
+import networkx as nx
 import numpy as np
 import scipy.sparse as sp
 import torch
-import torch.nn.functional as F
-import sys
-import pickle as pkl
-import networkx as nx
-import json
-from networkx.readwrite import json_graph
-import pdb
-import os
-import re
-import networkx as nx
-import numpy as np
-import scipy.sparse as sp
 import torch as th
-from sklearn.model_selection import ShuffleSplit
-import pickle as pkl
-import sys
-import networkx as nx
-import numpy as np
-import scipy.sparse as sp
-from numpy.linalg import matrix_power
 
 if torch.cuda.is_available():
-    from torch_geometric.datasets import CitationFull, Coauthor, Amazon, Flickr, WikiCS
     from torch_geometric.utils import to_dense_adj, contains_self_loops, remove_self_loops
-    
+
 sys.setrecursionlimit(99999)
 
 
@@ -37,14 +22,16 @@ def encode_onehot(labels):
                              dtype=np.int32)
     return labels_onehot
 
+
 def preprocess_features(features):
     """Row-normalize feature matrix and convert to tuple representation"""
     rowsum = np.array(features.sum(1))
-    r_inv = (1/rowsum).flatten()
+    r_inv = (1 / rowsum).flatten()
     r_inv[np.isinf(r_inv)] = 0.
     r_mat_inv = sp.diags(r_inv)
     features = r_mat_inv.dot(features)
     return features
+
 
 def load_data(dataset_str):
     """
@@ -82,12 +69,12 @@ def load_data(dataset_str):
     if dataset_str == 'citeseer':
         # Fix citeseer dataset (there are some isolated nodes in the graph)
         # Find isolated nodes, add them as zero-vecs into the right position
-        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
+        test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder) + 1)
         tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-        tx_extended[test_idx_range-min(test_idx_range), :] = tx
+        tx_extended[test_idx_range - min(test_idx_range), :] = tx
         tx = tx_extended
         ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-        ty_extended[test_idx_range-min(test_idx_range), :] = ty
+        ty_extended[test_idx_range - min(test_idx_range), :] = ty
         ty = ty_extended
 
     features = sp.vstack((allx, tx)).tolil()
@@ -99,15 +86,16 @@ def load_data(dataset_str):
 
     return adj, features, labels
 
+
 def load_torch_geometric_data(dataset, name):
     cur = os.getcwd()
 
     if dataset in {'WikiCS', 'Flickr'}:
         data = eval(dataset + "(root = '" + cur.replace("\\", "/") + "/torch_geometric_data/" + dataset + "')")
     else:
-        data = eval(dataset + "(root = '" + cur.replace("\\", "/") + "/torch_geometric_data/" + dataset + "'," + "name = '" + name + "')")
+        data = eval(dataset + "(root = '" + cur.replace("\\",
+                                                        "/") + "/torch_geometric_data/" + dataset + "'," + "name = '" + name + "')")
     # e.g. Coauthor(root='...', name = 'CS')
-
     edge = data[0].edge_index
     if contains_self_loops(edge):
         edge = remove_self_loops(edge)[0]
@@ -115,19 +103,19 @@ def load_torch_geometric_data(dataset, name):
 
     adj = to_dense_adj(edge)[0].numpy()
 
-    print("Nodes: %d, edges: %d, features: %d, classes: %d. \n"%(len(adj[0]), len(edge[0])/2, len(data[0].x[0]), len(np.unique(data[0].y))))
+    print("Nodes: %d, edges: %d, features: %d, classes: %d. \n" % (
+        len(adj[0]), len(edge[0]) / 2, len(data[0].x[0]), len(np.unique(data[0].y))))
 
     mask = np.transpose(adj) != adj
     col_sum = adj.sum(axis=0)
-    print("Check adjacency matrix is sysmetric: %r"%(mask.sum().item() == 0))
-    print("Check the number of isolated nodes: %d"%((col_sum == 0).sum().item()))
-    print("Node degree Max: %d, Mean: %.4f, SD: %.4f"%(col_sum.max(), col_sum.mean(), col_sum.std()))
+    print("Check adjacency matrix is sysmetric: %r" % (mask.sum().item() == 0))
+    print("Check the number of isolated nodes: %d" % ((col_sum == 0).sum().item()))
+    print("Node degree Max: %d, Mean: %.4f, SD: %.4f" % (col_sum.max(), col_sum.mean(), col_sum.std()))
 
     return adj, data[0].x.numpy(), data[0].y.numpy()
 
 
 def full_load_data(dataset_name):
-    #splits_file_path = 'splits/'+dataset_name+'_split_0.6_0.2_'+str(idx)+'.npz'
     if dataset_name in {'cora', 'citeseer', 'pubmed'}:
         adj, features, labels = load_data(dataset_name)
         labels = np.argmax(labels, axis=-1)
@@ -148,8 +136,7 @@ def full_load_data(dataset_name):
         G = nx.DiGraph().to_undirected()
         graph_node_features_dict = {}
         graph_labels_dict = {}
-        
-        
+
         if dataset_name == 'film':
             with open(graph_node_features_and_labels_file_path) as graph_node_features_and_labels_file:
                 graph_node_features_and_labels_file.readline()
@@ -170,9 +157,7 @@ def full_load_data(dataset_name):
                     assert (int(line[0]) not in graph_node_features_dict and int(line[0]) not in graph_labels_dict)
                     graph_node_features_dict[int(line[0])] = np.array(line[1].split(','), dtype=np.uint8)
                     graph_labels_dict[int(line[0])] = int(line[2])
-                    
-                    
-                    
+
         with open(graph_adjacency_list_file_path) as graph_adjacency_list_file:
             graph_adjacency_list_file.readline()
             for line in graph_adjacency_list_file:
@@ -187,39 +172,28 @@ def full_load_data(dataset_name):
                 G.add_edge(int(line[0]), int(line[1]))
 
         adj = nx.adjacency_matrix(G, sorted(G.nodes()))
-        #adj = adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
         features = np.array(
             [features for _, features in sorted(G.nodes(data='features'), key=lambda x: x[0])])
         labels = np.array(
             [label for _, label in sorted(G.nodes(data='label'), key=lambda x: x[0])])
     features = preprocess_features(features)
 
-    g = adj
-  
-    # with np.load(splits_file_path) as splits_file:
-    #     train_mask = splits_file['train_mask']
-    #     val_mask = splits_file['val_mask']
-    #     test_mask = splits_file['test_mask']
-    
-    num_features = features.shape[1]
-    num_labels = len(np.unique(labels))
     assert (np.array_equal(np.unique(labels), np.arange(len(np.unique(labels)))))
 
     features = th.FloatTensor(features)
     labels = th.LongTensor(labels)
-    # train_mask = th.BoolTensor(train_mask)
-    # val_mask = th.BoolTensor(val_mask)
-    # test_mask = th.BoolTensor(test_mask)
 
-    g = normalize(g+sp.eye(g.shape[0]))
+    g = adj
+    g = normalize(g + sp.eye(g.shape[0]))
     g_high = sp.eye(g.shape[0]) - g
     g = sparse_mx_to_torch_sparse_tensor(g)
     g_high = sparse_mx_to_torch_sparse_tensor(g_high)
-    
-    return g, g_high, features, labels #, train_mask, val_mask, test_mask
+
+    return g, g_high, features, labels
+
 
 def data_split(idx, dataset_name):
-    splits_file_path = 'splits/'+dataset_name+'_split_0.6_0.2_'+str(idx)+'.npz'
+    splits_file_path = 'splits/' + dataset_name + '_split_0.6_0.2_' + str(idx) + '.npz'
     with np.load(splits_file_path) as splits_file:
         train_mask = splits_file['train_mask']
         val_mask = splits_file['val_mask']
@@ -229,10 +203,11 @@ def data_split(idx, dataset_name):
     test_mask = th.BoolTensor(test_mask)
     return train_mask, val_mask, test_mask
 
+
 def normalize(mx):
     """Row-normalize sparse matrix"""
     rowsum = np.array(mx.sum(1))
-    r_inv = (1/rowsum).flatten()
+    r_inv = (1 / rowsum).flatten()
     r_inv[np.isinf(r_inv)] = 0.
     r_mat_inv = sp.diags(r_inv)
     mx = r_mat_inv.dot(mx)
@@ -255,6 +230,7 @@ def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     shape = torch.Size(sparse_mx.shape)
     return torch.sparse.FloatTensor(indices, values, shape)
 
+
 def parse_index_file(filename):
     """Parse index file."""
     index = []
@@ -262,15 +238,17 @@ def parse_index_file(filename):
         index.append(int(line.strip()))
     return index
 
+
 def sys_normalized_adjacency(adj):
-   adj = sp.coo_matrix(adj)
-   adj = adj + sp.eye(adj.shape[0])
-   row_sum = np.array(adj.sum(1))
-   row_sum=(row_sum==0)*1+row_sum
-   d_inv_sqrt = np.power(row_sum, -0.5).flatten()
-   d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
-   d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
-   return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
+    adj = sp.coo_matrix(adj)
+    adj = adj + sp.eye(adj.shape[0])
+    row_sum = np.array(adj.sum(1))
+    row_sum = (row_sum == 0) * 1 + row_sum
+    d_inv_sqrt = np.power(row_sum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return d_mat_inv_sqrt.dot(adj).dot(d_mat_inv_sqrt).tocoo()
+
 
 def normalize_adj(adj):
     """Symmetrically normalize adjacency matrix."""
@@ -281,38 +259,41 @@ def normalize_adj(adj):
     d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
     return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
 
-def normalize_tensor(mx, symmetric = 0):
+
+def normalize_tensor(mx, symmetric=0):
     """Row-normalize sparse matrix"""
-    rowsum = torch.sum(mx,1)
+    rowsum = torch.sum(mx, 1)
     if symmetric == 0:
         r_inv = torch.pow(rowsum, -1).flatten()
         r_inv[torch.isinf(r_inv)] = 0.
         r_mat_inv = torch.diag(r_inv)
         mx = torch.mm(r_mat_inv, mx)
         return mx
-        
+
     else:
         r_inv = torch.pow(rowsum, -0.5).flatten()
         r_inv[torch.isinf(r_inv)] = 0.
         r_mat_inv = torch.diag(r_inv)
-        mx = torch.mm(torch.mm(r_mat_inv, mx),r_mat_inv)
+        mx = torch.mm(torch.mm(r_mat_inv, mx), r_mat_inv)
         return mx
-    
+
+
 def dataset_edge_balance(adj, labels):
     """Measure the edge balance of each dataset"""
     num_class = labels.max().item() + 1
     num_nodes = labels.size()[0]
     num_nodes_per_class = np.zeros(num_class)
-    edge_balance_per_class = np.zeros([num_class,2])
+    edge_balance_per_class = np.zeros([num_class, 2])
     for i in range(num_class):
-        idx = np.where(labels.numpy()==i)[0]
+        idx = np.where(labels.numpy() == i)[0]
         num_nodes_per_class[i] = idx.shape[0]
-        edge_balance_per_class[i,0] = np.sum(adj[idx,:][:,idx])
-        edge_balance_per_class[i,1] = np.sum(adj[idx, :][:, np.delete(np.arange(num_nodes), idx) ]) 
-    
+        edge_balance_per_class[i, 0] = np.sum(adj[idx, :][:, idx])
+        edge_balance_per_class[i, 1] = np.sum(adj[idx, :][:, np.delete(np.arange(num_nodes), idx)])
+
     return num_nodes_per_class, edge_balance_per_class
 
-def random_disassortative_splits(labels, num_classes, training_percentage = 0.6):
+
+def random_disassortative_splits(labels, num_classes, training_percentage=0.6):
     # * 0.6 labels for training
     # * 0.2 labels for validation
     # * 0.2 labels for testing
@@ -322,25 +303,17 @@ def random_disassortative_splits(labels, num_classes, training_percentage = 0.6)
         index = torch.nonzero((labels == i)).view(-1)
         index = index[torch.randperm(index.size(0))]
         indices.append(index)
-    percls_trn = int(round(training_percentage*(labels.size()[0]/num_classes)))
-    val_lb = int(round(0.2*labels.size()[0]))
-    # train_index = torch.cat([i[:int(len(i)*0.6)] for i in indices], dim=0)
-    train_index = torch.cat([i[:percls_trn] for i in indices], dim=0)
+    percls_trn = int(round(training_percentage * (labels.size()[0] / num_classes)))
+    val_lb = int(round(0.2 * labels.size()[0]))
 
-    # val_index = torch.cat([i[int(len(i)*0.6):int(len(i)*0.8)] for i in indices], dim=0)
-    # test_index = torch.cat([i[int(len(i)*0.8):] for i in indices], dim=0)
+    train_index = torch.cat([i[:percls_trn] for i in indices], dim=0)
     rest_index = torch.cat([i[percls_trn:] for i in indices], dim=0)
     rest_index = rest_index[torch.randperm(rest_index.size(0))]
-    
 
     train_mask = index_to_mask(train_index, size=labels.size()[0])
-    
-    # val_mask = index_to_mask(val_index, size=labels.size()[0])
-    # test_mask = index_to_mask(test_index, size=labels.size()[0])
-    
     val_mask = index_to_mask(rest_index[:val_lb], size=labels.size()[0])
     test_mask = index_to_mask(rest_index[val_lb:], size=labels.size()[0])
-    
+
     return train_mask, val_mask, test_mask
 
 
